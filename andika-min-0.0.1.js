45bbfd1bc6a21e602eb19b4abf40b6c5fe71e619
nyingi <nyingimaina@gmail.com>
2022-09-09 10:58:16 +0300

    Switched to using attribute 'data-holder' instead of element id, so as to allow multiple elements to share the same place holder value

--- andika-min-0.0.1.js
+++ andika-min-0.0.1.js
@@ -1,24 +1,18 @@
-const _0x2a83d8 = _0x24cb;
-function _0x24cb(t, e) {
-  const n = _0x52c2();
-  return (_0x24cb = function (t, e) {
-    return n[(t -= 385)];
-  })(t, e);
-}
-!(function (t, e) {
-  const n = _0x24cb,
-    o = _0x52c2();
+const _0x48fd4c = _0x974b;
+!(function (t, n) {
+  const e = _0x974b,
+    o = _0x5ef8();
   for (;;)
     try {
       if (
-        410182 ===
-        (-parseInt(n(391)) / 1) * (-parseInt(n(387)) / 2) +
-          parseInt(n(403)) / 3 +
-          parseInt(n(385)) / 4 +
-          (-parseInt(n(400)) / 5) * (-parseInt(n(402)) / 6) +
-          parseInt(n(392)) / 7 +
-          -parseInt(n(399)) / 8 +
-          (-parseInt(n(414)) / 9) * (parseInt(n(418)) / 10)
+        285486 ===
+        -parseInt(e(391)) / 1 +
+          parseInt(e(370)) / 2 +
+          (parseInt(e(363)) / 3) * (parseInt(e(398)) / 4) +
+          (-parseInt(e(375)) / 5) * (-parseInt(e(366)) / 6) +
+          (-parseInt(e(393)) / 7) * (parseInt(e(362)) / 8) +
+          (-parseInt(e(383)) / 9) * (-parseInt(e(382)) / 10) +
+          -parseInt(e(397)) / 11
       )
         break;
       o.push(o.shift());
@@ -26,121 +20,159 @@ function _0x24cb(t, e) {
       o.push(o.shift());
     }
 })();
-const _0x53e191 = document.getElementById(_0x2a83d8(413)),
-  _0x5b5978 = JSON[_0x2a83d8(419)](_0x53e191.value);
-function _0x3f00be() {
-  const t = _0x2a83d8;
+const _0x36f829 = JSON[_0x48fd4c(360)](
+    document[_0x48fd4c(367)](_0x48fd4c(376)).value
+  ),
+  _0x10589b = _0x48fd4c(399);
+function _0x50fc8a() {
+  const t = _0x48fd4c;
   try {
-    _0xd5a9f2();
-  } catch (e) {
-    const n = _0x265b87(document, t(396));
-    if (!n) throw e;
+    _0x55e2f4();
+  } catch (n) {
+    const e = _0x398ab1(document, "body");
+    if (!e) throw n;
     {
-      const o = document.createElement(t(404));
-      o.setAttribute("style", t(389)),
-        (o[t(397)] = e + ""),
-        n.appendChild(o),
-        console[t(398)](e);
+      const o = document[t(372)](t(364));
+      o[t(396)](
+        t(365),
+        "color:#870000; background:#f5a6a6; padding:7px; border-radius:10px;"
+      ),
+        (o[t(374)] = n + ""),
+        e[t(380)](o),
+        console[t(388)](n);
     }
   }
 }
-function _0xd5a9f2() {
-  const t = _0x2a83d8;
-  for (var e in _0x5b5978)
-    if (_0x5b5978[t(408)](e) && typeof _0x5b5978[e] !== t(412)) {
-      const n = document[t(388)](e);
-      if (n) {
-        const o = Reflect.get(_0x5b5978, e) ?? "";
-        Array.isArray(o) ? _0xe9fb8(e, o) : (n[t(397)] = o);
-      } else console.warn(t(407));
+function _0x55e2f4() {
+  const t = _0x48fd4c;
+  for (var n in _0x36f829)
+    if (_0x36f829[t(402)](n) && typeof _0x36f829[n] !== t(371)) {
+      const e = _0x560c47(n);
+      if (e && e.length > 0)
+        for (let o = 0; o < e[t(377)]; o++) {
+          const r = e[o];
+          if ((console.log("" + n), console[t(392)](r), r)) {
+            const e = Reflect[t(390)](_0x36f829, n) ?? "";
+            Array[t(385)](e) ? _0x324c4f(n, e) : (r[t(374)] = e);
+          } else console[t(387)](t(381));
+        }
     }
 }
-function _0xe9fb8(t, e) {
-  const n = _0x2a83d8;
-  if (!e || 0 === e[n(401)]) return void console[n(410)](n(405) + t + n(395));
-  const o = document[n(388)](t);
-  if (!o || !1 === _0x4b130f(o, n(417))) throw new Error(n(411) + t + n(416));
-  for (let t = 0; t < o[n(393)].length; t++)
-    if (_0x4b130f(o[n(393)][t], "tr")) {
-      return void _0xdac9c8(o, o[n(393)][t], e);
+function _0x324c4f(t, n) {
+  const e = _0x48fd4c;
+  if (!n || 0 === n[e(377)]) return void console.warn(e(369) + t + e(368));
+  const o = _0x560c47(t);
+  if (o && o[e(377)] > 0)
+    for (let r = 0; r < o[e(377)]; r++) {
+      const c = o[r];
+      if (!c || !1 === _0x5909d8(c, "tbody"))
+        throw new Error(
+          e(373) + t + "' but it is missing. Cannot tabulate data."
+        );
+      for (let t = 0; t < c[e(386)][e(377)]; t++)
+        if (_0x5909d8(c.childNodes[t], "tr")) {
+          return void _0x2b7e62(c, c[e(386)][t], n);
+        }
+      throw new Error(e(379) + t + e(401));
     }
-  throw new Error("Found tbody tag with id " + t + n(390));
 }
-function _0x52c2() {
-  const t = [
-    "color:#870000; background:#f5a6a6; padding:7px; border-radius:10px;",
-    " but no template row. Please add a tr with child td tags that match the property names of the elements of the array",
-    "18sKsxsS",
-    "5673507GDsrjZ",
-    "childNodes",
-    "appendChild",
-    "' has no data.",
-    "body",
-    "innerHTML",
-    "error",
-    "2015256ZRmJNb",
-    "115KWTiUo",
-    "length",
-    "111966ONVeru",
-    "2429772lEyimc",
-    "div",
-    "Array with key '",
-    "toLocaleLowerCase",
-    "Could not find an element with id '{key}'. A value might not be populated",
-    "hasOwnProperty",
-    "tagName",
-    "warn",
-    "Expected to find tbody tag with id '",
-    "function",
-    "json",
-    "17105004wxpKAP",
-    "get",
-    "' but it is missing. Cannot tabulate data.",
-    "tbody",
-    "10wGDVtk",
-    "parse",
-    "531644IwUYVO",
-    "cloneNode",
-    "42234cEqSjS",
-    "getElementById",
-  ];
-  return (_0x52c2 = function () {
-    return t;
-  })();
-}
-function _0xdac9c8(t, e, n) {
-  const o = _0x2a83d8;
-  for (let r = 0; r < n.length; r++) {
-    const a = n[r],
-      c = e[o(386)]();
-    for (let t = 0; t < e.childNodes[o(401)]; t++) {
-      const n = e[o(393)][t];
-      if ((n.id && _0x4b130f(n, "td")) || _0x4b130f(n, "th")) {
-        const t = a[o(408)](n.id),
-          e = n.cloneNode();
-        (e.id = "" + e.id + r),
-          t && (e.innerHTML = Reflect[o(415)](a, n.id) ?? ""),
-          c[o(394)](e);
+function _0x2b7e62(t, n, e) {
+  const o = _0x48fd4c;
+  for (let r = 0; r < e[o(377)]; r++) {
+    const c = e[r],
+      f = n[o(384)]();
+    for (let t = 0; t < n[o(386)][o(377)]; t++) {
+      const e = n[o(386)][t];
+      if (
+        (_0x7f14d2(e, _0x10589b) && _0x5909d8(e, "td")) ||
+        _0x5909d8(e, "th")
+      ) {
+        const t = e.cloneNode(),
+          n = e[o(400)][_0x10589b]?.[o(361)];
+        if (n) {
+          c.hasOwnProperty(n) && (t[o(374)] = Reflect[o(390)](c, n) ?? "");
+        }
+        f[o(380)](t);
       }
     }
-    t[o(394)](c);
+    t[o(380)](f);
   }
-  e.remove();
+  n[o(394)]();
 }
-function _0x265b87(t, e) {
-  const n = _0x2a83d8;
+function _0x974b(t, n) {
+  const e = _0x5ef8();
+  return (_0x974b = function (t, n) {
+    return e[(t -= 360)];
+  })(t, n);
+}
+function _0x398ab1(t, n) {
+  const e = _0x48fd4c;
   if (!t) return null;
-  if (_0x4b130f(t, e)) return t;
-  for (let o = 0; o < t.childNodes.length; o++) {
-    const r = _0x265b87(t[n(393)][o], e);
+  if (_0x5909d8(t, n)) return t;
+  for (let o = 0; o < t[e(386)][e(377)]; o++) {
+    const r = _0x398ab1(t[e(386)][o], n);
     if (r) return r;
   }
   return null;
 }
-function _0x4b130f(t, e) {
-  const n = _0x2a83d8;
-  return (
-    !!(t && t.tagName && e) && t[n(409)][n(406)]() === e.toLocaleLowerCase()
-  );
+function _0x5ef8() {
+  const t = [
+    "139238pEBnBd",
+    "log",
+    "1561iftGUi",
+    "remove",
+    "toLocaleLowerCase",
+    "setAttribute",
+    "2219217lKKQmF",
+    "1950496jOOZcB",
+    "data-holder",
+    "attributes",
+    " but no template row. Please add a tr with child td tags that match the property names of the elements of the array",
+    "hasOwnProperty",
+    "parse",
+    "textContent",
+    "12440rrresM",
+    "3NVPBmb",
+    "div",
+    "style",
+    "1028526ZQCGjC",
+    "getElementById",
+    "' has no data.",
+    "Array with key '",
+    "36932XfwfhB",
+    "function",
+    "createElement",
+    "Expected to find tbody tag with id '",
+    "innerHTML",
+    "10zgBnFv",
+    "json",
+    "length",
+    "querySelectorAll",
+    "Found tbody tag with id ",
+    "appendChild",
+    "Could not find an element with id '{key}'. A value might not be populated",
+    "20nPqDIi",
+    "559368AaWFrx",
+    "cloneNode",
+    "isArray",
+    "childNodes",
+    "warn",
+    "error",
+    "tagName",
+    "get",
+  ];
+  return (_0x5ef8 = function () {
+    return t;
+  })();
 }
-_0x3f00be();
+function _0x560c47(t) {
+  return document[_0x48fd4c(378)]("[" + _0x10589b + "='" + t + "']");
+}
+function _0x7f14d2(t, n) {
+  return !!(t && t[_0x48fd4c(400)] && t.attributes[n]);
+}
+function _0x5909d8(t, n) {
+  const e = _0x48fd4c;
+  return !!(t && t[e(389)] && n) && t[e(389)][e(395)]() === n[e(395)]();
+}
+_0x50fc8a();
